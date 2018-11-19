13464eda64d0e1345eb79c715ac281c70cc47e73
tastyCanOfMalk <eddytheflow@gmail.com>
2018-11-18 19:59:57 -0500

    more readme fixes

--- analysis01.R
+++ analysis01.R
@@ -10,16 +10,16 @@ if (!require(ggthemes)) install.packages("ggthemes")
 library(ggthemes)
 if (!require(gridExtra)) install.packages("gridExtra")
 library(gridExtra)
+if (!require(ggridges)) install.packages("ggridges")
+library(ggridges)
 
 if (!require(arulesViz)) install.packages("arulesViz")
 library(arulesViz)
 if (!require(arules)) install.packages("arules")
 library(arules)
-if (!require(ggridges)) install.packages("ggridges")
-library(ggridges)
 
-# setwd("/home/e/R/bakery.sales/")
-setwd("C:/Users/e/Documents/R/bakery.sales.MBA")
+setwd("/home/e/R/bakery.sales/")
+# setwd("C:/Users/e/Documents/R/bakery.sales.MBA")
 
 # Import our csv and look at the structure
   # Some things to note: each item occupies one row, 
@@ -35,7 +35,7 @@ difftime(x$Date[nrow(x)],x$Date[1])
 ## EXPLORATION
 
 # Most popular Items
-x %>% 
+x1 <- x %>% 
   group_by(Item) %>% 
   summarise(Count = n()) %>% 
   arrange(desc(Count)) %>%
@@ -45,10 +45,11 @@ x %>%
   coord_flip()+
   ggtitle("Most popular items")+
   theme(legend.position="none")
-  
+x1
+
 # Charting # sales per date shows obvious peaks and valleys,
 # There seems to be a decreased amount of sold items on weekdays with peaks on weekends
-x1 <- x %>% 
+x2 <- x %>% 
   group_by(Date) %>% 
   summarise(Count= n()) %>% 
   mutate(Day=wday(Date,label=T)) %>% 
@@ -57,9 +58,10 @@ x1 <- x %>%
   geom_bar(stat="identity")+
   ggtitle("Line items sold per day")+
   scale_fill_viridis(discrete=TRUE)
+x2
 
 # We can see the obvious top sales day is Saturday, followed by Sun/Mon
-x2 <- x %>% 
+x3 <- x %>% 
   mutate(Day = wday(Date,label=T)) %>% 
   group_by(Day) %>% 
   summarise(Count= n()) %>% 
@@ -68,9 +70,10 @@ x2 <- x %>%
   geom_bar(stat="identity")+
   ggtitle("Total line items sold per weekday")+
   theme(legend.position="none")
-  
+x3
+
 # Grouping by unique transactions rather than line items tells a similar story
-x3 <- x %>% 
+x4 <- x %>% 
   mutate(wday=wday(Date,label=T)) %>% 
   group_by(wday,Transaction) %>% 
   summarise(n_distinct(Transaction)) %>% 
@@ -80,11 +83,41 @@ x3 <- x %>%
   geom_bar(stat="identity")+
   ggtitle("Total unique transactions per weekday")+
   theme(legend.position="none")
+x4
+
+# We see some difference in scale which means, unsurprisingly,
+# that each purchase is not just one single item.
+# This does bring to mind a question of how many items are purchased per transaction,
+# Let's graph this versus day of the week as we've done above
+x5.1 <- x %>% 
+  mutate(Day = wday(Date,label=T)) %>% 
+  group_by(Day) %>% 
+  summarise(Count= n()) 
+
+x5.2 <- x %>% 
+  mutate(wday=wday(Date,label=T)) %>% 
+  group_by(wday,Transaction) %>% 
+  summarise(n_distinct(Transaction)) %>% 
+  summarise(Count=n())
+
+x5.3 <- data.frame(x5.1, # Days, total items
+                   x5.2[2], # unique transactions
+                   x5.1[2]/x5.2[2])  # items per unique transaction
+colnames(x5.3) <- c("Day","Line","Unique","Items.Trans")
+
+x5 <- 
+  ggplot(x5.3,aes(x=Day,y=Items.Trans,fill=Day))+
+  theme_fivethirtyeight()+
+  geom_bar(stat="identity")+
+  ggtitle("Total unique transactions per weekday")+
+  theme(legend.position="none")+
+  geom_text(aes(label=round(Items.Trans,1)), vjust=2)
+x5
 
 # If we sort by the top sales days of all time and group by weekday
 # the pattern becomes a little more easy to see, with Sunday being much
 # more likely to take top item sales
-x4 <- x %>% 
+x6 <- x %>% 
   group_by(Date) %>% 
   summarise(Count= n()) %>% 
   arrange(desc(Count)) %>% 
@@ -93,34 +126,78 @@ x4 <- x %>%
   ggplot(aes(x=wday,y=Count,fill=wday))+
   theme_fivethirtyeight()+
   geom_bar(stat="identity")+
-  # scale_x_discrete(limits=c("Mon","Tue","Wed","Thu","Fri","Sat","Sun"))+
   ggtitle("Top 50 sales days, grouped by weekday")+
   theme(legend.position="none")
-
-grid.arrange(x1,x2,x3,x4)
+x6 
 
 # We can also take a look at time data
-x3 <- x %>%
+# We see most items are sold just before noon
+x7 <- x %>%
   mutate(Hour = as.factor(hour(x$Time))) %>% 
   group_by(Hour) %>% 
   summarise(Count=n()) %>% 
   ggplot(aes(x=Hour,y=Count,fill=Hour))+
+  theme_fivethirtyeight()+
   geom_bar(stat="identity")+
+  ggtitle("Line items sold per Hour")+
   theme(legend.position="none")
+x7
 
-# joyplot of above data for each different weekday
-x4 <- x %>% 
-  mutate(Hour = hour(Time),
-         Day = as.factor(wday(Date))) %>% 
-  group_by(Hour, Day) %>% 
+# Again, let's look at unique transactions
+# And again we see the scale change while the pattern remains
+x8 <- x %>% 
+  mutate(Hour = as.factor(hour(Time)))%>% 
+  group_by(Hour,Transaction) %>% 
+  summarise(n_distinct(Transaction)) %>% 
   summarise(Count=n()) %>% 
-  ggplot(aes(x=Hour,y=Day,fill=Day,height=..density..))+
-  stat_density_ridges(scale=2,quantile_lines = TRUE,alpha=1)+
-  scale_fill_viridis(discrete = TRUE)+
-  scale_x_continuous(limits=c(1,24),
-                     breaks=seq(1,24,1))+
+  ggplot(aes(x=Hour,y=Count,fill=Hour))+
+  theme_fivethirtyeight()+
+  geom_bar(stat="identity")+
+  ggtitle("Unique transactions per Hour")+
   theme(legend.position="none")
-  ggtitle("Line items sold per hour per day (1-Mon,7-Sun)")
+x8
+
+# We might as well also look into how many items per transaction occur
+x9.1 <- x %>% 
+  mutate(Hour = as.factor(hour(Time)))%>% 
+  group_by(Hour) %>% 
+  summarise(Count= n()) 
+
+x9.2 <- x %>% 
+  mutate(Hour = as.factor(hour(Time)))%>% 
+  group_by(Hour,Transaction) %>% 
+  summarise(n_distinct(Transaction)) %>% 
+  summarise(Count=n())
+
+x9.3 <- data.frame(x9.1, # Days, total items
+                   x9.2[2], # unique transactions
+                   x9.1[2]/x9.2[2])  # items per unique transaction
+colnames(x9.3) <- c("Hour","Line","Unique","Items.Trans")
+
+x9 <- 
+  ggplot(x9.3,aes(x=Hour,y=Items.Trans,fill=Hour))+
+  theme_fivethirtyeight()+
+  geom_bar(stat="identity")+
+  ggtitle("Total items per transaction per hour")+
+  theme(legend.position="none")+
+  geom_text(aes(label=round(Items.Trans,1)), vjust=2)
+x9
+
+# ridge plot of above data for each different weekday
+# We can easily see where the most dense transaction times are this way,
+# in essence this sort of summarizes all the data we've messed with above
+x10 <- x %>% 
+  mutate(Hour=as.factor(hour(Time)),
+         Day=wday(Date,label=T)) %>% 
+  group_by(Day,Hour,Transaction) %>% 
+  summarise(Count=n()) %>% 
+  ggplot(aes(x=Hour,y=Day,group=Day,fill=..density..))+
+  theme_fivethirtyeight()+
+  ggtitle("Transaction density per Hour by Day")+
+  theme(legend.position="none")+
+  geom_density_ridges_gradient(scale=2, rel_min_height=0.01)+
+  scale_fill_viridis(option="magma")
+x10
 
 ## MARKET BASKET ANALYSIS
 # now that we've explored a little bit, we can get to the market basket analysis
